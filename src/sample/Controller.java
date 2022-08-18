d74af5bf287582329022c8f36c7d577704b96bbb
himal999 <himal.official99@gmail.com>
2022-08-18 18:50:39 +0530

    complete app

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -66,7 +66,7 @@ public class Controller implements Initializable {
                     Text text = new Text(messageToSend);
                     TextFlow textFlow = new TextFlow(text);
 
-                    textFlow.setStyle("-fx-color:rgb(239,242,255)" + "-fx-background-color:rgb(15,125,242)"+"-fx-background-radius:20px");
+                    textFlow.setStyle("-fx-color:rgb(239,242,255);" + "-fx-background-color:rgb(15,125,242);"+"-fx-background-radius:20px");
 
                     textFlow.setPadding(new Insets(5,10,5,10));
                     text.setFill(Color.color(0.934,0.945,0.99600));
@@ -89,7 +89,7 @@ public class Controller implements Initializable {
         Text text = new Text(messageFromClient);
         TextFlow textFlow = new TextFlow(text);
 
-        textFlow.setStyle("-fx-background-color:rgb(233,233,235)"+"-fx-background-radius:20px");
+        textFlow.setStyle("-fx-background-color:rgb(233,233,235);" + "-fx-background-radius:20px");
         textFlow.setPadding(new Insets(5,10,5,10));
         hBox.getChildren().add(textFlow);
 
